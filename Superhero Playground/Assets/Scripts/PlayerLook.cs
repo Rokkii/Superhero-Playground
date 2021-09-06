b7e695e668694b494f1d5d84fda5b60c2188208d
Neil Alexander <neilalex94@gmail.com>
2021-09-06 22:16:26 +0100

    Scene Setup, Movement & Camera Setup
    - Setup basic "lab" scene for testing
    - Added code for player movement
    - Added code for player camera control

---
+++ Superhero Playground/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    Vector2 lookMouse;
+    Vector2 smoothMove;
+
+    [SerializeField]
+    private float sensitivity;
+
+    [SerializeField]
+    private float smoothing;
+
+    [SerializeField]
+    private GameObject character;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Find change in mouse - mouseDelta
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        // Multiply mouseDelta by sensitivity and smoothing values set in client
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        // Use Lerp to move camera smoothly on x + y axis
+        smoothMove.x = Mathf.Lerp(smoothMove.x, mouseDelta.x, 1f / smoothing);
+        smoothMove.y = Mathf.Lerp(smoothMove.y, mouseDelta.y, 1f / smoothing);
+
+        // Add smooth movement calculated to direction mouse looking
+        lookMouse += smoothMove;
+
+        // Use inverted value of lookMouse to control camera on right axis (up/down)
+        transform.localRotation = Quaternion.AngleAxis(-lookMouse.y, Vector3.right);
+
+        // Use value of lookMouse to rotate the player character obj, moving the camera + player on the x-axis
+        character.transform.localRotation = Quaternion.AngleAxis(lookMouse.x, character.transform.up);
+    }
+}
