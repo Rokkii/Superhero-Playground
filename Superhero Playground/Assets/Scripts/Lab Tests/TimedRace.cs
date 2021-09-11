ca2ea6b03bf83c356613500def9b03da109760b0
Neil Alexander <neilalex94@gmail.com>
2021-09-11 10:54:32 +0100

    Fixed Array Error, Added Race Reset, Race Complete UI, RaceTriggerArea
    - Fixed issue where after player had collided with all race markers, array length error would appear
    - Added race reset functionality within script, allowing multiple play attempts by the player
    - Added race complete UI displaying player time with continue button, enabled via script
    - Added in-game timer board to display the last time the player took to complete the race
    - Repositioned TestTrack markers in test environment
    - Added new RaceTriggerArea game object and tag in order for player to trigger an in-game prompt to start the race when colliding with object
    - Added trigger enter and exit to TimedRace script to enable and disable the race start prompt when the player enters and leaves race area

--- Superhero Playground/Assets/Scripts/Lab Tests/TimedRace.cs
+++ Superhero Playground/Assets/Scripts/Lab Tests/TimedRace.cs
@@ -9,6 +9,18 @@ public class TimedRace : MonoBehaviour
     private GameObject raceTimerText;
 
     [SerializeField]
+    private GameObject raceCompleteUI;
+
+    [SerializeField]
+    private GameObject finalRaceTimeText;
+
+    [SerializeField]
+    private GameObject inGameTimerBoard;
+
+    [SerializeField]
+    private GameObject raceTriggerPrompt;
+
+    [SerializeField]
     private float timePassed = 0f;
 
     [SerializeField]
@@ -26,6 +38,7 @@ public class TimedRace : MonoBehaviour
         raceTimerText.SetActive(true);
         raceTimerText.GetComponent<Text>().text = timePassed.ToString("0.0");
         Debug.Log("Number of race markers: " + raceTrackMarkers.Length);
+        
         // Set first RaceMarker game object to enabled
         raceTrackMarkers[currentMarker].SetActive(true);
     }
@@ -36,15 +49,6 @@ public class TimedRace : MonoBehaviour
         // Start timer counting up, update timer text per Update() loop
         timePassed += 1 * Time.deltaTime;
         raceTimerText.GetComponent<Text>().text = timePassed.ToString("0.0");
-
-        // If player has collided with all RaceMarkers, end race, reset currentMarker to 0
-        if (currentMarker >= raceTrackMarkers.Length)
-        {
-            Debug.Log("Race Complete! Time Taken: " + timePassed);
-            currentMarker = 0;
-            // Disable TimedRace script when race complete
-            gameObject.GetComponent<TimedRace>().enabled = false;
-        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -58,6 +62,38 @@ public class TimedRace : MonoBehaviour
 
             // Change current RaceMarker to next marker and enable game object
             currentMarker++;
+
+            // If player has collided with all RaceMarkers, end race, reset currentMarker + timer to 0
+            if (currentMarker >= raceTrackMarkers.Length)
+            {
+                Debug.Log("Race Complete! Time Taken: " + timePassed);
+
+                // Update finalRaceTimeText to give player the time taken to complete race
+                finalRaceTimeText.GetComponent<Text>().text = timePassed.ToString("0.0");
+
+                // Update text of the in-game time board to display the completed race time
+                inGameTimerBoard.GetComponent<Text>().text = timePassed.ToString("0.0");
+
+                // Enable raceCompleteUI to display race time to player
+                raceCompleteUI.SetActive(true);
+
+                currentMarker = 0;
+                timePassed = 0;
+
+                // Reset nextRaceMarker to currentMarker, which will be 0
+                nextRaceMarker = raceTrackMarkers[currentMarker];
+
+                // Reset timer UI text to 0 and hide
+                raceTimerText.SetActive(false);
+                raceTimerText.GetComponent<Text>().text = timePassed.ToString("0.0");
+
+                // Disable TimedRace script when race complete
+                gameObject.GetComponent<TimedRace>().enabled = false;
+                
+                // Once reset all race components, exit race loop
+                return;
+            }
+
             raceTrackMarkers[currentMarker].SetActive(true);
 
             // Set next RaceMarker to the next RaceMarker in the array
@@ -67,6 +103,28 @@ public class TimedRace : MonoBehaviour
         {
             // Display error message if player hits wrong race marker
             Debug.Log("Player hit wrong race marker! At time: " + timePassed);
+        }
+    }
+
+    // Trigger race start prompt if the player enters race area
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "RaceTriggerArea")
+        {
+            // Trigger prompt to start the race if player enters race area
+            Debug.Log("Player entered race area!");
+            raceTriggerPrompt.SetActive(true);
+        }
+    }
+
+    // Disable race start prompt if the player leaves race area
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "RaceTriggerArea")
+        {
+            // Trigger prompt to start the race if player enters race area
+            Debug.Log("Player left race area!");
+            raceTriggerPrompt.SetActive(false);
         }
     }
 }
